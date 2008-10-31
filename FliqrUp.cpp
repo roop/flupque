525e971a13dd5f2b2680ddd1dcbe98b75d672b78
Roopesh Chander <roop@forwardbias.in>
2008-11-01 01:30:42 +0530

    Have to expose as slots those functions we want to access in the javascript

--- FliqrUp.cpp
+++ FliqrUp.cpp
@@ -3,23 +3,52 @@
 #include <QtWebKit> 
 #include <QFileDialog>
 
+class FuFileDialog : public QFileDialog {
+    Q_OBJECT
+public:
+    FuFileDialog(QWidget * parent = 0, Qt::WindowFlags flags = 0)
+        : QFileDialog(parent, flags) {
+        setFileMode(QFileDialog::ExistingFiles);
+        setViewMode(QFileDialog::List);
+        setDirectory(QDir::homePath() + "/Pictures");
+    }
+
+public slots:
+   void setFileMode(QFileDialog::FileMode fileMode) {
+       QFileDialog::setFileMode(fileMode);
+   }
+   void addNameFilter(QString filter) {
+       QStringList filters = nameFilters();
+       filters.append(filter);
+       setNameFilters(filters);
+   }
+   void clearNameFilters() {
+       QFileDialog::setNameFilters(QStringList());
+   }
+   void setViewMode(QFileDialog::ViewMode viewMode) {
+       QFileDialog::setViewMode(viewMode);
+   }
+   void setDirectory(const QString& dir) {
+       QFileDialog::setDirectory(dir);
+   }
+   QStringList selectedFiles() const {
+       return QFileDialog::selectedFiles();
+   }
+};
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    QFileDialog qFileDialog;
-    qFileDialog.setFileMode(QFileDialog::ExistingFiles);
-    qFileDialog.setNameFilter("Photos (*.jpg *.tiff)");
-    qFileDialog.setViewMode(QFileDialog::List);
-    qFileDialog.setDirectory(QDir::homePath() + "/Pictures");
-    qDebug() << "labelText(3) = " << qFileDialog.labelText((QFileDialog::DialogLabel)3);
+    FuFileDialog fuFileDialog;
 
     QWebView *view = new QWebView();
     view->load(QUrl("html/main.html"));
     view->show();
 
-    view->page()->mainFrame()->addToJavaScriptWindowObject("qFileDialog", &qFileDialog);
+    view->page()->mainFrame()->addToJavaScriptWindowObject("fuFileDialog", &fuFileDialog);
 
     return app.exec();
 }
 
+#include "FliqrUp.moc"
--- scripts/fileDialog.js
+++ scripts/fileDialog.js
@@ -1,10 +1,14 @@
 
 var fileDialog = {
     add_photo: function() {
-        var qFileDialog = window.qFileDialog;
-        if (qFileDialog.exec()) {
+        var fuFileDialog = window.fuFileDialog;
+        fuFileDialog.clearNameFilters();
+        fuFileDialog.addNameFilter("Photos (*.jpg *.tiff)");
+        fuFileDialog.addNameFilter("Images (*.jpg *.png *.gif *.bmp)");
+        fuFileDialog.addNameFilter("All files (*.*)");
+        if (fuFileDialog.exec()) {
             // alert("Got you");
-            alert("files selected");
+            alert("files selected " + fuFileDialog.selectedFiles());
         } else {
             alert("Nothing to do!");
         }
