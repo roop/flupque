ad96bb05f07a34c2bb2cb844e2e80eebe8be54ab
Roopesh Chander <roop@forwardbias.in>
2008-11-02 16:14:30 +0530

    Add license/copyright everywhere

--- main.cpp
+++ main.cpp
@@ -1,3 +1,17 @@
+/*
+ * This file is part of the Flupque software
+ *
+ * Copyright (c) 2008 Roopesh Chander <roop@forwardbias.in>
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (GPL), version 2
+ * only.  This library is distributed WITHOUT ANY WARRANTY, whether
+ * express or implied. See the GNU GPL for more details.
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ */
+
+
 #include <QApplication>
 #include <QMainWindow>
 #include <QtWebKit> 
