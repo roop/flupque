fcecf2ab50c861b2e316431ea3eee24081c2eddc
Roopesh Chander <roop@forwardbias.in>
2008-11-02 22:34:50 +0530

    Let junta select/unselect a photo by clicking on it

--- js/photo.js
+++ js/photo.js
@@ -16,6 +16,7 @@ var photo = {
     // Member variables
     photolist: [],
     photocount: 0,
+    selectedcount: 0,
 
     // Methods
     add_dialog: function() {
@@ -34,7 +35,7 @@ var photo = {
 
     add_photo: function(path) {
         var id = this.photocount++;
-        this.photolist[id] = path;
+        this.photolist[id] = new Photo(path, id);
 
         // hide the "Drag photos here" message in the middle of the window
         document.getElementById('photos_init').style.display = 'none';
@@ -48,6 +49,7 @@ var photo = {
         var li = document.createElement('li');
         li.id = 'photo' + id;
         img.name = 'photo' + id;
+        img.id = id;
         li.appendChild(img);
         var list = document.getElementById('photos_list');
         list.insertBefore(li, list.firstChild);
@@ -58,6 +60,35 @@ var photo = {
                        );
     },
 
+    select_photo: function(id) {
+        document.images['photo' + id].className = 'selected';
+        this.photolist[id].is_selected = true;
+        this.selectedcount++;
+        if (this.selectedcount == 1) {
+            this.enable_photo_actions();
+        }
+    },
+
+    unselect_photo: function(id) {
+        document.images['photo' + id].className = '';
+        this.photolist[id].is_selected = false;
+        this.selectedcount--;
+        if (this.selectedcount == 0) {
+            this.disable_photo_actions();
+        }
+    },
+
+    enable_photo_actions: function() {
+        document.getElementById('t_remove').className = 'button';
+        document.getElementById('t_rotate_l').className = 'enabled';
+        document.getElementById('t_rotate_r').className = 'enabled';
+    },
+
+    disable_photo_actions: function() {
+        document.getElementById('t_remove').className = 'disabled_button';
+        document.getElementById('t_rotate_l').className = 'disabled';
+        document.getElementById('t_rotate_r').className = 'disabled';
+    }
 };
 
 // The show_thumbnail function shall get called once the
@@ -70,5 +101,22 @@ function show_thumbnail(id, path, width, height) {
     img.width = width;
     img.height = height;
     img.style.visibility = 'visible';
+    img.onclick = function() { image_clicked(img.name); };
+}
+
+function Photo(path, id) {
+    this.path = path;
+    this.id = id;
+    this.name = name;
+    this.is_selected = false;
+}
+
+function image_clicked(imgname) {
+    img = document.images[imgname];
+    if (img.className == 'selected') {
+        photo.unselect_photo(img.id);
+    } else {
+        photo.select_photo(img.id);
+    }
 }
 
